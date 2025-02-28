8e8272d8631e702000ca9c79e1e292c739932212
Kyrylo Dyachenko <104459315+kirill25460@users.noreply.github.com>
2025-02-28 19:47:40 +0200

    start Guarantees page

--- app/guarantees/page.js
+++ app/guarantees/page.js
@@ -2,13 +2,13 @@
 
 import Header from "../../components/header/header";
 import Footer from "../../components/Footer/footer";
-
+import Guarantees from "../../components/guarantees/guarantees";
 import styles from "./page.module.css";
 export default function GuaranteesPage() {
   return (
     <div className={styles.container}>
       <Header />
- 
+ <Guarantees/>
       <Footer />
     </div>
   );
