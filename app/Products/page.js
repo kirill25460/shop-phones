af6cf9b816f69c47d7f324c6bb0756955b12dff1
Ponedilok Danyyl <danya253545@gmail.com>
2025-04-17 23:21:58 +0300

    prodectPage

--- app/Products/page.js
+++ app/Products/page.js
@@ -1,40 +1,16 @@
 "use client";
-import Link from "next/link";
 
-import { useEffect, useState } from "react";
 import Header from "../../components/header/header";
 import Footer from "../../components/Footer/footer";
-import SideBarProducts from "../../components/sideBarProducts/sideBarProducts";
-import ProductsList from "../../components/Products/productsList";
-import ProductsTitle from "../../components/ProductsTitle/ProductsTitle";
 import styles from "./page.module.css";
+import ProductInfo from "@/components/ProductInfo/ProductInfo";
+
 export default function ProductsPage() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    fetch("/products.json")
-      .then((res) => res.json())
-      .then((data) => setProducts(data));
-  }, []);
-
-  return (<>
+  return (
     <div className={styles.container}>
       <Header />
-      <ProductsTitle />
-      <div className={styles.containerProducts}>
-        <SideBarProducts />
-        <ul className={styles.containerList}>
-        {products.map((product) => (
-            <li key={product.id} className={styles.productItem}>
-              <Link href={`/product/${product.id}`}>
-                <ProductsList product={product} />
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <ProductInfo />
+      <Footer />
     </div>
-    <Footer />
-    </>
   );
 }
--- components/offerOfTheDay/offerOfTheDay.jsx
+++ components/offerOfTheDay/offerOfTheDay.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./offerOfTheDay.module.css";
 
 import mainPic from "../../public/offerOfTheDayPic.webp";
+import Link from "next/link";
 
 export default function OfferOfTheDay() {
   return (
@@ -10,13 +11,18 @@ export default function OfferOfTheDay() {
         <h2 className={styles.nameIphone}>Пропозиція дня</h2>
         <h2 className={styles.title}>
           Apple iPhone 15 Pro 256GB
-          <br />Natural Titanium
+          <br />
+          Natural Titanium
         </h2>
         <h4 className={styles.price}>4,290.00 zł</h4>
-        <button className={styles.mainButton}>Придбати</button>
+        <Link href="/products">
+          <button className={styles.mainButton}>Придбати</button>
+        </Link>
       </div>
       <div className={styles.imageWrapper}>
-        <Image src={mainPic} alt="mainpic" width={376} height={445} priority />
+        <Link href="/products">
+          <Image src={mainPic} alt="mainpic" priority />
+        </Link>
       </div>
     </div>
   );
